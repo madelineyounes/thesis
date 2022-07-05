d643ab4e1e25d07bd7112fee38c3e41aaedc303a
younem <mmyounes@iinet.net.au>
2022-07-05 10:33:30 +1000

    fixed csv added to run script

--- run_umbrellaDID.py
+++ run_umbrellaDID.py
@@ -122,7 +122,7 @@ base_cache_fp = "/srv/scratch/z5208494/cache/huggingface/datasets/"
 # Dataset name and filename of the csv file containing the training data
 # For generating filepath to file location
 train_name = "umbrella_alldevdata"
-train_filename = "data_1file"
+train_filename = "data_1file2"
 print("train_name:", train_name)
 print("train_filename:", train_filename)
 
