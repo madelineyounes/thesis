5891a2afaa01157a28ad744ea3cce6635e6c0f44
younem <mmyounes@iinet.net.au>
2022-07-05 18:15:10 +1000

    swtiched preprocess

--- run_umbrellaDID.py
+++ run_umbrellaDID.py
@@ -410,7 +410,17 @@ def audio_to_array_fn(batch):
             pass
             #print("File " + batch["id"] + ".wav not found in test or training.")
 
-encoded_data = data.map(audio_to_array_fn, num_proc=4)
+
+def preprocess_function(examples):
+    audio_arrays = [x["array"] for x in examples["audio"]]
+    inputs = feature_extractor(
+        audio_arrays,
+        sampling_rate=feature_extractor.sampling_rate,
+    )
+    return inputs
+
+
+encoded_data = data.map(preprocess_function, num_proc=4, batched=True)
 print(encoded_data)
 # Check a few rows of data to verify data properly loaded
 print("--> Verifying data with a random sample...")
@@ -607,6 +617,7 @@ training_args = TrainingArguments(
     output_dir=model_fp,
     evaluation_strategy=set_evaluation_strategy,
     per_device_train_batch_size=set_per_device_train_batch_size,
+    gradient_checkpointing=True,
     gradient_accumulation_steps=set_gradient_accumulation_steps,
     learning_rate=set_learning_rate,
     weight_decay=set_weight_decay,
