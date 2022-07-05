2754d80b5cffe9ad5e847789953d4d9fc9c32ecb
younem <mmyounes@iinet.net.au>
2022-07-05 16:26:51 +1000

    fixed training

--- run_umbrellaDID.py
+++ run_umbrellaDID.py
@@ -228,7 +228,7 @@ set_save_steps = 1000                         # Default = 500
 print("save_steps:", set_save_steps)
 set_save_total_limit = 40                   # Optional
 print("save_total_limit:", set_save_total_limit)
-set_fp16 = True                             # Default = False
+set_fp16 = False                             # Default = False
 print("fp16:", set_fp16)
 set_eval_steps = 1000                         # Optional
 print("eval_steps:", set_eval_steps)
