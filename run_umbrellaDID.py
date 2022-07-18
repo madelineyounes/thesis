7c0e97480a4f9dc2c5f9a571c3e5819c970bbebe
younem <mmyounes@iinet.net.au>
2022-07-18 14:08:30 +1000

    switched from CTC to standard trainer

--- run_umbrellaDID.py
+++ run_umbrellaDID.py
@@ -210,7 +210,7 @@ set_evaluation_strategy = "epoch"           # Default = "no"
 print("evaluation strategy:", set_evaluation_strategy)
 set_per_device_train_batch_size = 4         # Default = 8
 print("per_device_train_batch_size:", set_per_device_train_batch_size)
-set_gradient_accumulation_steps = 4         # Default = 4
+set_gradient_accumulation_steps = 2         # Default = 4
 print("gradient_accumulation_steps:", set_gradient_accumulation_steps)
 set_learning_rate = 0.00004                 # Default = 0.00005
 print("learning_rate:", set_learning_rate)
@@ -851,8 +851,6 @@ training_args = TrainingArguments(
     output_dir=model_fp,
     evaluation_strategy=set_evaluation_strategy,
     per_device_train_batch_size=set_per_device_train_batch_size,
-    per_device_eval_batch_size=set_per_device_train_batch_size,
-    gradient_checkpointing=True,
     gradient_accumulation_steps=set_gradient_accumulation_steps,
     learning_rate=set_learning_rate,
     weight_decay=set_weight_decay,
@@ -880,7 +878,7 @@ training_args = TrainingArguments(
 # All instances can be passed to Trainer and
 # we are ready to start training!
 model.gradient_checkpointing_enable()
-trainer = CTCTrainer(
+trainer = Trainer(
     model=model,
     data_collator=data_collator,
     args=training_args,
