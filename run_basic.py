8d6f1afdde7a99b1c189e695802baf0b6d279886
younem <mmyounes@iinet.net.au>
2022-10-05 21:53:23 +1100

    increased batch size

--- run_basic.py
+++ run_basic.py
@@ -208,7 +208,7 @@ print("\n------> TRAINING ARGUMENTS... ----------------------------------------\
 # For setting training_args = TrainingArguments()
 set_evaluation_strategy = "no"           # Default = "no"
 print("evaluation strategy:", set_evaluation_strategy)
-set_per_device_train_batch_size = 4         # Default = 8
+set_per_device_train_batch_size = 10         # Default = 8
 print("per_device_train_batch_size:", set_per_device_train_batch_size)
 set_gradient_accumulation_steps = 2         # Default = 4
 print("gradient_accumulation_steps:", set_gradient_accumulation_steps)
