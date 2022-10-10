d5a9d375e93f690680d661194f7cffbd23d29f33
younem <mmyounes@iinet.net.au>
2022-10-11 09:36:27 +1100

    added plot script

--- support/plotfiles.py
+++ support/plotfiles.py
@@ -1,17 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
+f1_path = ""
+f2_path = ""
+f3_path = ""
+headers = ['Name', 'Age', 'Marks']
 
-
-f1 = ""
-f2 = ""
-f3 = ""
+df = pd.read_csv('student.csv', names=headers)
 x = np.arange(10)
 
 plt.plot(x, x)
 plt.plot(x, 2 * x)
 plt.plot(x, 3 * x)
 plt.plot(x, 4 * x)
+
+plt.title("Amount of Training and Test Files")
+plt.xlabel("X axis label")
+plt.ylabel("Y axis label")
 
 plt.legend(['y = x', 'y = 2x', 'y = 3x', 'y = 4x'], loc='upper left')
 
