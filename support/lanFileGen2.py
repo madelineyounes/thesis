f450655a42f19940fcef378b4cc21b168ee4eb36
younem <mmyounes@iinet.net.au>
2022-10-12 14:13:07 +1100

    created file to generte csv for voxlingua dataset

---
+++ support/lanFileGen2.py
@@ -0,0 +1,48 @@
+import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_confusion_matrix
+import numpy as np
+
+"""
+Program that generates training and test csvs for the VoxLingua107 language data
+"""
+
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+source_dir = os.path.join(ROOT_DIR, 'data')
+output_path = "../data/"
+data_label_path = "/srv/scratch/z5208494/dataset/VoxLingua107/"
+
+
+output_path = "../data/"
+
+test_l_out_file = output_path +"test_lan_50f.csv"
+train_l_out_file = output_path+ "train_lan_50f.csv"
+
+lan = ['ar', 'en', 'fr', 'it']
+
+num_files = 50 
+
+firstline = "id,label\n"
+ftest = open(test_l_out_file, 'w')
+ftest.write(firstline)
+
+ftrain = open(train_l_out_file, 'w')
+ftrain.write(firstline)
+
+for l in lan:
+    directory = data_label_path + l
+    trcount = 0
+    tscount = 0
+    for filename in os.scandir(directory):
+        if filename.is_file() and trcount < num_files:
+            ftrain.write(filename + f",{l}\n")
+            trcount += 1
+
+        if filename.is_file() and tscount < num_files:
+            ftest.write(filename + f",{l}\n")
+            tscount += 1
+
+ftrain.close()
+ftest.close()
+print("done")
+
