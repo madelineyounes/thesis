1f33979910bd29f97749698bed1026bfcd679eca
younem <mmyounes@iinet.net.au>
2022-10-12 12:47:39 +1100

    added lr test

--- run_lr0001.py
+++ run_lr0001.py
@@ -202,7 +202,7 @@ batch_size = 12        # Default = 8
 print("batch_size:", batch_size)
 set_gradient_accumulation_steps = 2         # Default = 4
 print("gradient_accumulation_steps:", set_gradient_accumulation_steps)
-set_learning_rate = 0.00004                 # Default = 0.00005
+set_learning_rate = 0.0001                 # Default = 0.00005
 print("learning_rate:", set_learning_rate)
 set_weight_decay = 0.01                     # Default = 0
 print("weight_decay:", set_weight_decay)
