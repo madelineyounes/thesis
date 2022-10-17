b3781cc30f7fe5ce533ce41c5d6cae4d9c6040c0
younem <mmyounes@iinet.net.au>
2022-10-17 20:59:40 +1100

    added file imports

---
+++ support/lanFileGen3.py
@@ -0,0 +1,58 @@
+import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_confusion_matrix
+import numpy as np
+
+"""
+Program that takes in original datafiles and limits them to an amount of files per dialect
+"""
+
+output_path = "../data/"
+
+input_file = output_path + "adi17_offical_train.txt"
+
+out_u_file = output_path +"imported_u_train_files.csv"
+out_r_file = output_path + "imported_r_train_files.csv"
+
+dialect_dict = {
+    "EGY": ['EGY', 'SDN'],
+    "GLF": ['IRQ', 'KWT', 'ARE', 'QAT', 'OMN', 'SAU', 'YEM'],
+    "LEV": ['PSE', 'LBN', 'SYR', 'JOR'],
+    "NOR": ['MRT', 'MAR', 'DZA', 'LBY']
+}
+
+umbrella_dialects = ['NOR', 'EGY', 'GLF', 'LEV']
+
+num_files = 500 
+
+
+lines = tuple(open(input_file, 'r'))
+
+firstline = "id,label\n"
+fUout = open(out_u_file, 'w')
+fUout.write(firstline)
+
+fRout = open(out_r_file, 'w')
+fRout.write(firstline)
+
+for d in umbrella_dialects:
+    numreg = len(dialect_dict[d])
+    numf = np.floor(num_files/numreg)
+    for rd in dialect_dict[d]:
+        dcount = 0
+        rdcount = 0
+        for line in lines:
+            if rd in line.rstrip("\n"):
+                if dcount < num_files:
+                    filename = line.split(' ')[0]
+                    fUout.write(filename + f",{d}\n")
+                    dcount += 1
+                if rdcount < num_files:
+                    filename = line.split(' ')[0]
+                    fRout.write(filename + f",{rd}\n")
+                    rdcount += 1
+
+fRout.close()
+fUout.close()
+print("done")
+
---
+++ support/transferFiles.py
@@ -0,0 +1,32 @@
+import os
+from paramiko import SSHClient
+from scp import SCPClient
+
+ssh = SSHClient()
+ssh.load_system_host_keys()
+
+server = 'z5208494@katana.restech.unsw.edu.au'
+username = 'z5208494'
+password = 'Hobbitb0i369'
+
+target_path = "/srv/scratch/z5208494"
+local_path = ""
+
+with SCPClient(ssh.get_transport()) as scp:
+    scp.put('my_file.txt', 'my_file.txt')  # Copy my_file.txt to the server
+
+ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+ssh.connect(server, username=username, password=password)
+sftp = ssh.open_sftp()
+
+
+out_u_file = "../data/imported_u_train_files.csv"
+lines = tuple(open(out_u_file, 'r'))
+
+for line in lines:
+    file = line.split(' ')[0]
+    localpath = target_path + file + ".wav"
+    remotepath = local_path + file + ".wav"
+    sftp.put(localpath, remotepath)
+sftp.close()
+ssh.close()
