ee600833acbb7f80522c129f9a167cbd1f672d27
younem <mmyounes@iinet.net.au>
2022-10-29 00:32:29 +1100

    changed layer

--- run_cnn_downstream.py
+++ run_cnn_downstream.py
@@ -456,14 +456,14 @@ model = Wav2Vec2ForSequenceClassification.from_pretrained(
     model_name, ignore_mismatched_sizes=True)
 model.classifier = nn.Sequential(
     nn.Conv2d(in_channels=256, out_channels=128,  kernel_size=(5, 5)),
-    nn.AvgPool2d(kernel_size=(5, 5)),
+    nn.AvgPool2d(kernel_size=(3, 3)),
     nn.Conv2d(in_channels=64, out_channels=10, kernel_size=(3, 3)),
     nn.AvgPool2d(kernel_size=(3, 3)),
     nn.Flatten(),
     nn.Linear(10, 120),
     nn.Linear(120, 84),
-    nn.Linear(84, 17),
-    nn.Linear(in_features=17, out_features=num_labels, bias=True)
+    nn.Linear(84, 4),
+    nn.Linear(in_features=4, out_features=num_labels, bias=True)
 )
 
 
