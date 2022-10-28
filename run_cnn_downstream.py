835cca7638f8207c5fa6227f9bf09f4984a452ed
younem <mmyounes@iinet.net.au>
2022-10-29 00:44:13 +1100

    changfed layers

--- run_cnn_downstream.py
+++ run_cnn_downstream.py
@@ -455,15 +455,19 @@ print("--> Loading pre-trained checkpoint...")
 model = Wav2Vec2ForSequenceClassification.from_pretrained(
     model_name, ignore_mismatched_sizes=True)
 model.classifier = nn.Sequential(
-    nn.Conv2d(in_channels=256, out_channels=128,  kernel_size=(5, 5)),
-    nn.AvgPool2d(kernel_size=(3, 3)),
-    nn.Conv2d(in_channels=64, out_channels=10, kernel_size=(3, 3)),
-    nn.AvgPool2d(kernel_size=(3, 3)),
-    nn.Flatten(),
-    nn.Linear(10, 120),
+    nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+    nn.BatchNorm2d(6),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size = 2, stride = 2),
+    nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+    nn.BatchNorm2d(16),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size = 2, stride = 2),
+    nn.Linear(400, 120),
+    nn.ReLU(),
     nn.Linear(120, 84),
-    nn.Linear(84, 4),
-    nn.Linear(in_features=4, out_features=num_labels, bias=True)
+    nn.ReLU(),
+    nn.Linear(84, num_labels, bias=True)
 )
 
 
